d163a607d2896eb5ab743619b3c81e47c6190d29
Robin <robin.godtschalck@hotmail.fr>
2024-07-13 18:03:38 +0200

    chore: Update dependencies and add lazy loading for images

--- game/main.js
+++ game/main.js
@@ -1,17 +1,25 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set( 0, 30, 25 );
+controls.update();
+camera.lookAt(0, 0, 0);
+
+console.log(scene);
 
 const loader = new GLTFLoader();
 
-let paddle1, paddle2, ball, topWall, bottomWall, boundry, plane;
+let paddle1, paddle2, ball, topWall, bottomWall, boundry, scoreP1, scoreP2, scoreP1object = [], scoreP2object = [], p1WIN, p2WIN;
 
 // Charger les modèles
 loader.load('models/model.glb', function(gltf) {
@@ -19,39 +27,102 @@ loader.load('models/model.glb', function(gltf) {
 
     // Récupérer les objets de la scène glTF
     paddle1 = gltf.scene.getObjectByName('Paddle');
-    paddle2 = gltf.scene.getObjectByName('Paddle.001');
+    paddle2 = gltf.scene.getObjectByName('Paddle001');
     ball = gltf.scene.getObjectByName('Ball');
     topWall = gltf.scene.getObjectByName('Wall');
-    bottomWall = gltf.scene.getObjectByName('Wall.001');
+    bottomWall = gltf.scene.getObjectByName('Wall001');
     boundry = gltf.scene.getObjectByName('Boundry');
-    plane = gltf.scene.getObjectByName('Plane');
+    gltf.scene.getObjectByName('Plane').visible = false;
+    p1WIN = gltf.scene.getObjectByName('P1WIN');
+    p2WIN = gltf.scene.getObjectByName('P2WIN');
+    scoreP1object.push(gltf.scene.getObjectByName('0_L'));
+    scoreP1object.push(gltf.scene.getObjectByName('1_L'));
+    scoreP1object.push(gltf.scene.getObjectByName('2_L'));
+    scoreP2object.push(gltf.scene.getObjectByName('0_R'));
+    scoreP2object.push(gltf.scene.getObjectByName('1_R'));
+    scoreP2object.push(gltf.scene.getObjectByName('2_R'));
 
+    // Positionner les scores
+    scoreP1object[0].visible = true;
+    scoreP1object[1].visible = false;
+    scoreP1object[2].visible = false;
+    scoreP2object[0].visible = true;
+    scoreP2object[1].visible = false;
+    scoreP2object[2].visible = false;
+    scoreP1 = 0;
+    scoreP2 = 0;
+    p1WIN.visible = false;
+    p2WIN.visible = false;
+    
+
+    
     animate();
 }, undefined, function(error) {
     console.error(error);
 });
 
-let ballSpeed = { x: 0.05, y: 0.05 };
-let paddleSpeed = 0.1;
+//lights
+
+const light = new THREE.AmbientLight( 0x404040, 15 ); // soft white light
+scene.add( light );
+
+let ballSpeed = { x: 0.08, z: 0.08 };
+let paddleSpeed = 1;
 
 function animate() {
     requestAnimationFrame(animate);
+    controls.update();
 
     if (ball && paddle1 && paddle2) {
         // Déplacement de la balle
         ball.position.x += ballSpeed.x;
-        ball.position.y += ballSpeed.y;
+        ball.position.z += ballSpeed.z;
+        
 
-        // Collision avec les murs
-        if (ball.position.y >= 2.4 || ball.position.y <= -2.4) {
-            ballSpeed.y = -ballSpeed.y;
+        //Collision avec les murs
+        if (ball.position.z <= topWall.position.z + 0.5 || ball.position.z >= bottomWall.position.z - 0.5) {
+            ballSpeed.z *= -1;
         }
+        console.log(ball.position.z, topWall.position.z);
 
         // Collision avec les raquettes
-        if ((ball.position.x >= 2.4 && ball.position.y >= paddle2.position.y - 0.5 && ball.position.y <= paddle2.position.y + 0.5) ||
-            (ball.position.x <= -2.4 && ball.position.y >= paddle1.position.y - 0.5 && ball.position.y <= paddle1.position.y + 0.5)) {
-            ballSpeed.x = -ballSpeed.x;
+        if (ball.position.x <= paddle1.position.x + 1 && ball.position.z <= paddle1.position.z + 1 && ball.position.z >= paddle1.position.z - 1) {
+            ballSpeed.x *= -1;
         }
+        if (ball.position.x >= paddle2.position.x - 1 && ball.position.z <= paddle2.position.z + 1 && ball.position.z >= paddle2.position.z - 1) {
+            ballSpeed.x *= -1;
+        }
+        // Point marqué
+        if (ball.position.x <= paddle1.position.x){
+            ball.position.set(0, 0, 0);
+            ball.position.x -= ballSpeed.x;
+            ball.position.z -= ballSpeed.z;
+            scoreP2++;
+            scoreP2object[scoreP2 - 1].visible = false;
+            scoreP2object[scoreP2].visible = true;
+        }
+        else if (ball.position.x >= paddle2.position.x){
+            ball.position.set(0, 0, 0);
+            ball.position.x -= ballSpeed.x;
+            ball.position.z -= ballSpeed.z;
+            scoreP1++;
+            scoreP1object[scoreP1 - 1].visible = false;
+            scoreP1object[scoreP1].visible = true;
+        }
+        else if (scoreP1 == 2 || scoreP2 == 2){
+            ball.position.set(0, 0, 0);
+            if (scoreP1 == 2)
+                p1WIN.visible = true;
+            else
+                p2WIN.visible = true;
+        }
+    
+
+
+
+        console.log(ball.position.x, paddle2.position.x);
+     
+
     }
 
     renderer.render(scene, camera);
@@ -61,16 +132,16 @@ document.addEventListener('keydown', (event) => {
     if (paddle1 && paddle2) {
         switch (event.key) {
             case 'ArrowUp':
-                paddle2.position.y += paddleSpeed;
+                paddle2.position.z -= paddleSpeed;
                 break;
             case 'ArrowDown':
-                paddle2.position.y -= paddleSpeed;
+                paddle2.position.z += paddleSpeed;
                 break;
-            case 'w':
-                paddle1.position.y += paddleSpeed;
+            case 'z':
+                paddle1.position.z -= paddleSpeed;
                 break;
             case 's':
-                paddle1.position.y -= paddleSpeed;
+                paddle1.position.z += paddleSpeed;
                 break;
         }
     }
