28813bdb2eb9c4a565e0450d6feff1c77bbb02f2
Robin <robin.godtschalck@hotmail.fr>
2024-07-14 19:32:58 +0200

    refactor: Update model and lighting in main.js

--- game/main.js
+++ game/main.js
@@ -25,14 +25,18 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
 
 //Create a DirectionalLight and turn on shadows for the light
-const light = new THREE.DirectionalLight(0xFFA500, 5);
+const light = new THREE.DirectionalLight(0xFFFFFF, 10);
 light.position.set( -1, 1, -2 ); //default; light shining from top left
 light.castShadow = true; // default false
 scene.add( light );
+const light2 = new THREE.DirectionalLight(0xFFFFFF, 10);
+light2.position.set( 1, 1, -2 ); //default; light shining from top left
+light2.castShadow = true; // default false
+scene.add( light2 );
 let paddle1, paddle2, ball, plane, topWall, bottomWall, boundry, scoreP1, scoreP2, scoreP1object = [], scoreP2object = [], p1WIN, p2WIN;
 
 // Charger les modèles
-loader.load('models/model2.glb', function(gltf) {
+loader.load('models/modelSimpson.glb', function(gltf) {
     scene.add(gltf.scene);
 
     // Récupérer les objets de la scène glTF
@@ -40,12 +44,15 @@ loader.load('models/model2.glb', function(gltf) {
     plane.material.color.set(0x000000);
     plane.material.receiveShadow = true;
     plane.castShadow = true;
-    paddle1 = gltf.scene.getObjectByName('Paddle');
+
+    paddle1 = gltf.scene.getObjectByName('PaddleBart');
     paddle1.castShadow = true;
-    paddle2 = gltf.scene.getObjectByName('Paddle001');
+
+    paddle2 = gltf.scene.getObjectByName('PaddleMarge');
+
     paddle1.castShadow = true;
     paddle2.castShadow = true;
-    ball = gltf.scene.getObjectByName('Ball');
+    ball = gltf.scene.getObjectByName('donut');
     ball.castShadow = true;
     topWall = gltf.scene.getObjectByName('Wall');
     bottomWall = gltf.scene.getObjectByName('Wall001');
@@ -54,20 +61,63 @@ loader.load('models/model2.glb', function(gltf) {
     boundry = gltf.scene.getObjectByName('Boundry');
     p1WIN = gltf.scene.getObjectByName('P1WIN');
     p2WIN = gltf.scene.getObjectByName('P2WIN');
+
     scoreP1object.push(gltf.scene.getObjectByName('0_L'));
     scoreP1object.push(gltf.scene.getObjectByName('1_L'));
     scoreP1object.push(gltf.scene.getObjectByName('2_L'));
+    scoreP1object.push(gltf.scene.getObjectByName('3_L'));
+    scoreP1object.push(gltf.scene.getObjectByName('4_L'));
+    scoreP1object.push(gltf.scene.getObjectByName('5_L'));
+
     scoreP2object.push(gltf.scene.getObjectByName('0_R'));
     scoreP2object.push(gltf.scene.getObjectByName('1_R'));
     scoreP2object.push(gltf.scene.getObjectByName('2_R'));
+    scoreP2object.push(gltf.scene.getObjectByName('3_R'));
+    scoreP2object.push(gltf.scene.getObjectByName('4R'));
+    scoreP2object.push(gltf.scene.getObjectByName('5_R'));
+
+    gltf.scene.getObjectByName('Moustache_&_dance').visible = false;
+    gltf.scene.getObjectByName('Boundry').visible = false;
+    gltf.scene.getObjectByName('Boundry001').visible = false;
+    gltf.scene.getObjectByName('Boundry002').visible = false;
+    gltf.scene.getObjectByName('Boundry003').visible = false;
+    gltf.scene.getObjectByName('Boundry004').visible = false;
+    gltf.scene.getObjectByName('Boundry005').visible = false;
+    gltf.scene.getObjectByName('Boundry006').visible = false;
+    gltf.scene.getObjectByName('Boundry007').visible = false;
+    gltf.scene.getObjectByName('Boundry008').visible = false;
+    gltf.scene.getObjectByName('Boundry009').visible = false;
+    gltf.scene.getObjectByName('Boundry010').visible = false;
+    gltf.scene.getObjectByName('Boundry011').visible = false;
+    gltf.scene.getObjectByName('Boundry012').visible = false;
+    gltf.scene.getObjectByName('Boundry013').visible = false;
+    gltf.scene.getObjectByName('Boundry014').visible = false;
+    gltf.scene.getObjectByName('Boundry015').visible = false;
+    gltf.scene.getObjectByName('Boundry016').visible = false;
+    gltf.scene.getObjectByName('Boundry017').visible = false;
+    gltf.scene.getObjectByName('Ball').visible = false;
+    gltf.scene.getObjectByName('Paddle').visible = false;
+    gltf.scene.getObjectByName('Paddle001').visible = false;
+
+    
 
     // Positionner les scores
     scoreP1object[0].visible = true;
     scoreP1object[1].visible = false;
     scoreP1object[2].visible = false;
+    scoreP1object[3].visible = false;
+    scoreP1object[4].visible = false;
+    scoreP1object[5].visible = false;
+
     scoreP2object[0].visible = true;
     scoreP2object[1].visible = false;
     scoreP2object[2].visible = false;
+    scoreP2object[3].visible = false;
+    scoreP2object[4].visible = false;
+    scoreP2object[5].visible = false;
+    
+    
+
     scoreP1 = 0;
     scoreP2 = 0;
     p1WIN.visible = false;
@@ -128,9 +178,9 @@ function animate() {
             scoreP1object[scoreP1 - 1].visible = false;
             scoreP1object[scoreP1].visible = true;
         }
-        else if (scoreP1 == 2 || scoreP2 == 2){
+        else if (scoreP1 == 5 || scoreP2 == 5){
             ball.position.set(0, 0, 0);
-            if (scoreP1 == 2)
+            if (scoreP1 == 5)
                 p1WIN.visible = true;
             else
                 p2WIN.visible = true;
