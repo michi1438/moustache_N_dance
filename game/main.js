ef6f0dafb375c3650c09b2b1661229e8b121fb34
Robin <robin.godtschalck@hotmail.fr>
2024-07-19 12:40:36 +0200

    refactor: Update model, lighting, and ball speed in main.js

--- game/main.js
+++ game/main.js
@@ -9,7 +9,7 @@ showQuestion();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.shadowMap.enabled = true;
+
 const controls = new OrbitControls(camera, renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -21,38 +21,74 @@ camera.lookAt(0, 0, 0);
 console.log(scene);
 
 const loader = new GLTFLoader();
-let paddle1, paddle2, ball, plane, topWall, bottomWall, scoreP1, scoreP2, scoreP1object = [], scoreP2object = [], p1WIN, p2WIN;
+let paddle1, paddle2, ball, plane, topWall, bottomWall, scoreP1, scoreP2, scoreP1object = [], scoreP2object = [], p1WIN, p2WIN, title;
 
 //Lights & shadows
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-let dirLight = new THREE.DirectionalLight( 0xffffff, 2 );
-				dirLight.name = 'Dir. Light';
-				dirLight.position.set( 0, 10, 0 );
-				dirLight.castShadow = true;
-				dirLight.shadow.camera.near = 1;
-				dirLight.shadow.camera.far = 15;
-				dirLight.shadow.camera.right = 25;
-				dirLight.shadow.camera.left = - 25;
-				dirLight.shadow.camera.top	= 15;
-				dirLight.shadow.camera.bottom = - 15;
-				dirLight.shadow.mapSize.width = 1024;
-				dirLight.shadow.mapSize.height = 1024;
-				scene.add( dirLight );
+let dirLight = new THREE.DirectionalLight( 0xfffff0, 1 );
+dirLight.name = 'Dir. Light';
+dirLight.position.set( 0, 10, 0 );
+dirLight.castShadow = true;
+dirLight.shadow.camera.near = 1;
+dirLight.shadow.camera.far = 15;
+dirLight.shadow.camera.right = 25;
+dirLight.shadow.camera.left = - 25;
+dirLight.shadow.camera.top	= 15;
+dirLight.shadow.camera.bottom = - 15;
+dirLight.shadow.mapSize.width = 1024;
+dirLight.shadow.mapSize.height = 1024;
+scene.add( dirLight );
 
-scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+//scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
-scene.add(new THREE.AmbientLight(0x404040, 4));
+//scene.add( new THREE.AmbientLight( 0x404040, 3 ) );
+
+let spotLight = new THREE.SpotLight( 0xff6666, 10000 );
+spotLight.name = 'Spot Light';
+spotLight.angle = Math.PI / 5;
+spotLight.penumbra = 0.3;
+spotLight.position.set( 45, 10, 20);
+spotLight.castShadow = true;
+spotLight.shadow.camera.near = 8;
+spotLight.shadow.camera.far = 50;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+scene.add( spotLight );
+
+//scene.add( new THREE.CameraHelper( spotLight.shadow.camera ) );
+
+let spotLight2 = new THREE.SpotLight( 0x6666ff, 10000 );
+spotLight2.name = 'Spot Light';
+spotLight2.angle = Math.PI / 5;
+spotLight2.penumbra = 0.3;
+spotLight2.position.set( -45, 10, -20);
+spotLight2.castShadow = true;
+spotLight2.shadow.camera.near = 8;
+spotLight2.shadow.camera.far = 50;
+spotLight2.shadow.mapSize.width = 1024;
+spotLight2.shadow.mapSize.height = 1024;
+scene.add( spotLight2 );
+
+//scene.add( new THREE.CameraHelper( spotLight2.shadow.camera ) );
+
+//audio
+
+const audioLoader = new THREE.AudioLoader();
+
+const listener = new THREE.AudioListener();
+camera.add( listener );
 
 let isModelLoaded = false;
 let isConfigReady = false;
 let gameConfig = {};
 
-loader.load('models/modelSimpson.glb', function(gltf) {
+loader.load('models/modelMoustache.glb', function(gltf) {
     scene.add(gltf.scene);
 
     plane = gltf.scene.getObjectByName('Plane');
-    //plane.material.color.set(0x000);
+    plane.material.color.set(0x000);
+  
     //plane.material.receiveShadow = true;
     plane.receiveShadow = true;
 
@@ -63,15 +99,20 @@ loader.load('models/modelSimpson.glb', function(gltf) {
     paddle2.castShadow = true;
 
     ball = gltf.scene.getObjectByName('Ball');
-    // ball.castShadow = true;
+    ball.castShadow = true;
 
     topWall = gltf.scene.getObjectByName('WallT');
     bottomWall = gltf.scene.getObjectByName('WallB');
-    // topWall.castShadow = true;
-    // bottomWall.castShadow = true;
+    topWall.castShadow = true;
+    bottomWall.castShadow = true;
 
     p1WIN = gltf.scene.getObjectByName('P1WIN');
+    p1WIN.castShadow = true;
     p2WIN = gltf.scene.getObjectByName('P2WIN');
+    p2WIN.castShadow = true;
+
+    title = gltf.scene.getObjectByName('title');
+    title.castShadow = true;
 
     scoreP1object.push(gltf.scene.getObjectByName('0_L'));
     scoreP1object.push(gltf.scene.getObjectByName('1_L'));
@@ -87,7 +128,6 @@ loader.load('models/modelSimpson.glb', function(gltf) {
     scoreP2object.push(gltf.scene.getObjectByName('4_R'));
     scoreP2object.push(gltf.scene.getObjectByName('5_R'));
 
-
     scoreP1object[0].visible = true;
     scoreP1object[1].visible = false;
     scoreP1object[2].visible = false;
@@ -101,6 +141,11 @@ loader.load('models/modelSimpson.glb', function(gltf) {
     scoreP2object[3].visible = false;
     scoreP2object[4].visible = false;
     scoreP2object[5].visible = false;
+
+    for (let i = 0; i < scoreP1object.length; i++) {
+        scoreP1object[i].castShadow = true;
+        scoreP2object[i].castShadow = true;
+    }
 
     scoreP1 = 0;
     scoreP2 = 0;
@@ -133,16 +178,16 @@ let paddleSpeed = 1;
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
-    //ball.position.set(0, 0, 0);
 
     if (ball && paddle1 && paddle2) {
         ball.position.x += ballSpeed.x;
         ball.position.z += ballSpeed.z;
-
+        
+        //collision murs
         if (ball.position.z <= topWall.position.z + 0.5 || ball.position.z >= bottomWall.position.z - 0.5) {
             ballSpeed.z *= -1;
         }
-
+        //collision paddle1 et paddle2
         if (ball.position.x <= paddle1.position.x + 0.6 && ball.position.z <= paddle1.position.z + 6.2 / 2 && ball.position.z >= paddle1.position.z - 6.2 / 2) {
             ballSpeed.x *= -1;
         }
