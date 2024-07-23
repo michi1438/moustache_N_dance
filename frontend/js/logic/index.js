c84fa63bd87dd8f496dad9f261907fea8e0699fa
Blerim Kukaqi <bkukaqi@c1r4s1.42lausanne.ch>
2024-07-23 15:31:02 +0200

    Add background-color + Main-moustache

--- frontend/js/logic/index.js
+++ frontend/js/logic/index.js
@@ -5,10 +5,10 @@ function renderIndex() {
 			</div>
 			<div class="row">
 				<div class="col-6 d-flex justify-content-center">
-				<img src="/frontend/img/index42.png" alt="PONG X 42" style="max-width=600px; max-height=600px;" width=100% height=100% />
+				<img src=".png" alt="" style="max-width=600px; max-height=600px;" width=100% height=100% />
 				</div>
 				<div class="col-6 d-flex justify-content-center">
-				<img src="/frontend/img/index42_second.png" alt="PONG x 42" style="max-width=600px; max-height=600px;" width=100% height=100%/>
+				<img src="" alt="" style="max-width=600px; max-height=600px;" width=100% height=100%/>
 				</div>
 			</div>
 	`;
