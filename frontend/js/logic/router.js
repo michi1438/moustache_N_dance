954ebc57e474adaf718d6bcbb3ece059d2195587
Blerim Kukaqi <bkukaqi@c2r7s1.42lausanne.ch>
2024-07-24 17:48:14 +0200

    Change to Dark Nav Bar + Add Toggle btn

--- frontend/js/logic/router.js
+++ frontend/js/logic/router.js
@@ -24,14 +24,79 @@ const routes = {
 		title: "Main",
 		path: "/",
 		view: handleIndex.renderIndex,
-		//load: handleIndex.loadIndex,
-		//listener: handleIndex.listenerIndex
+		// load: handleIndex.loadIndex,
+		// listener: handleIndex.listenerIndex
 	},
-	"twoplayers": {
-		title: "2 Joueurs Local",
-		path: "/twoplayers/",
-		view: renderTwoPlayers,
-		// load: handleTwoPlayers.loadTwoPlayers,
-		// listener: handleTwoPlayers.listenerTwoPlayers
-	},
+
 };
+
+/**
+ * Router function
+ * @param {string} value - The value of the button that was clicked
+ * Get the page from the routes object, if it exists
+ * Call the load function of the page
+ * If the load function returns 1 (the user can access it), render the view of the page
+*/
+export default async function router(value) {
+
+	var page = routes[value];
+	document.getElementById("main__content").innerHTML = page.view();
+	if (!page)
+		return;
+
+	// if (await page.load() === 1) {
+	// 	document.getElementById("main__content").innerHTML = page.view();
+
+	// 	// document.getElementById("topbar__profile--username").textContent =
+	// 	// 	sessionStorage.getItem("username") ? sessionStorage.getItem("username") : "user";
+	// 	// document.getElementById("topbar__profile--avatar").src =
+	// 	// 	sessionStorage.getItem("avatar") ? sessionStorage.getItem("avatar") : "/frontend/img/person-circle-Bootstrap.svg";
+	// 	// document.getElementById("topbar__profile--avatar").alt =
+	// 	// 	sessionStorage.getItem("avatar") ? sessionStorage.getItem("username") + " avatar" : "temp avatar";
+
+	// 	document.title = page.title;
+
+	// 	window.history.pushState({}, "", page.path);
+
+	// 	page.listener();
+	// }
+	else {
+		console.log("page.load(): Error, redirect to login page");
+		// router("login");
+	}
+};
+
+/**
+ * Event listener for DOMContentLoaded event
+ * If the user is on the index page, index specific logic is executed
+ * Attach event listener to the 'logout' button
+ * Attach event listeners on all buttons with the class 'dropdown-item' i.e. all buttons that redirect to another "page"
+*/
+document.addEventListener("DOMContentLoaded", () => {
+
+	// if (window.location.search.split("=")[0] == "?code") {
+	// 	let code = window.location.search.split("=")[1];
+	// 	load42Profile(code);
+	// }
+
+	if (window.location.pathname === "/") {
+		loadIndex();
+	}
+
+	// document.getElementById("topbar__logout").addEventListener("click", (e) => {
+	// 	e.preventDefault();
+	// 	handleLogout();
+	// });
+
+	document.querySelectorAll(".nav__item").forEach(element => {
+		element.addEventListener("click", (e) => {
+			e.preventDefault();
+
+			if (element.value !== window.location.pathname.replaceAll("/", "")) {
+				router(element.value);
+			}
+		})
+	});
+});
+
+export { router }
