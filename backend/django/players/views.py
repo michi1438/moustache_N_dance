43d1bd6ebf815f4811a4a53bee9fe169c4c486d9
lzito <lzito@42lausanne.ch>
2024-07-25 01:26:22 +0200

    added register

--- backend/django/players/views.py
+++ backend/django/players/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 def login_user(request):
@@ -21,3 +22,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("DECONNEXION REUSSIE ! GG !"))
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration successful!"))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register_user.html', {
+        'form':form,
+        })
