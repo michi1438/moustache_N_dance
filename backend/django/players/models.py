65d9453155d644212c46bef727cd33ce80500dbb
lzito <lzito@42lausanne.ch>
2024-08-14 15:15:50 +0200

    changed fields from models.py

--- backend/django/players/models.py
+++ backend/django/players/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Player(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    pseudo = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    password1 = models.CharField(max_length=50)
+    password2 = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    #avatar = models.ImageField()
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.username
